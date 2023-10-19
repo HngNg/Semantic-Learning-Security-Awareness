122166f667bf2c5a80e4178b255767099e42510b
Ngo Luu Tan Hung <tanhung09brvt@gmail.com>
2023-10-19 16:26:06 +0700

    Trying out new noise formula for Eve's CNN Temporarily turn off Bob's CNN

--- semantic_extraction/MNIST.py
+++ semantic_extraction/MNIST.py
@@ -115,13 +115,19 @@ for snr_val in range (10, 11):
             out_square = np.square(x_np)
             aver = np.sum(out_square) / np.size(out_square)
 
-            # snr = 3  # dB
-            # snr = 10  # dB
+            # # snr = 3  # dB
+            # # snr = 10  # dB
             snr = snr_val  # dB
-            aver_noise = aver / 10 ** (snr / 10)
-            noise_bob = np.random.random(size=x_np.shape) * np.sqrt(aver_noise)
-            noise_eve = noise_bob + np.random.random(size=x_np.shape) * np.sqrt(aver_noise)
+
+            # aver_noise = aver / 10 ** (snr / 10)
+            # noise_bob = np.random.random(size=x_np.shape) * np.sqrt(aver_noise)
+            # noise_eve = noise_bob + np.random.random(size=x_np.shape) * np.sqrt(aver_noise)
+
             # noise = noise.to(device)
+            noise_var = 10**(-snr / 10.0)
+
+            # Generate Gaussian noise with the calculated variance
+            noise_eve = np.random.normal(0, np.sqrt(noise_var), x_np.shape)
 
             x_np = x_np + noise_eve
             x = torch.from_numpy(x_np)
