505f7f12f35f334c761176bf9f2e185da26356c1
Ngo Luu Tan Hung <tanhung09brvt@gmail.com>
2023-11-01 15:57:46 +0700

    fix bug

--- semantic_extraction/CIFAR.py
+++ semantic_extraction/CIFAR.py
@@ -508,14 +508,14 @@ for lambda_var in range(1):
                     # imageio.imwrite(path, merged2)
                     # print('saved %s' % path)
 
-                    path = os.path.join('images/im-epoch-%d-lambda-%d-compre-%d.png' % (
+                    path = os.path.join('Semantic-Learning-Security-Awareness/semantic_extraction/images/im-epoch-%d-lambda-%d-compre-%d.png' % (
                         e, lambda1, compression_rate))
                     # scipy.misc.imsave(path, merged)
                     cv2.imwrite(path, im_data[0].transpose(1, 2, 0))
                     if not cv2.imwrite(path,  im_data[0].transpose(1, 2, 0)):
                         raise Exception("Could not write image")
                     #
-                    path = os.path.join('images/out-epoch-%d-lambda-%d-compre-%d.png' % (
+                    path = os.path.join('Semantic-Learning-Security-Awareness/semantic_extraction/images/out-epoch-%d-lambda-%d-compre-%d.png' % (
                         e, lambda1, compression_rate))
                     # scipy.misc.imsave(path, merged)
                     cv2.imwrite(path, out_data[0].transpose(1, 2, 0))
