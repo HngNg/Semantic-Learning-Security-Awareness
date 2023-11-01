e9ca5ca7a3cdddef3006f2da70bdae67e2c2e26d
Ngo Luu Tan Hung <tanhung09brvt@gmail.com>
2023-11-01 17:25:08 +0700

    zzzz

--- semantic_extraction/CIFAR.py
+++ semantic_extraction/CIFAR.py
@@ -508,8 +508,15 @@ for lambda_var in range(1):
                     path = os.path.join('Semantic-Learning-Security-Awareness/semantic_extraction/images/im-epoch-%d-lambda-%d-compre-%d.png' % (
                         e, lambda1, compression_rate))
                     # scipy.misc.imsave(path, merged)
-                    imageio.imwrite(path, im_data)
+                    imageio.imwrite(path, im_data[0].transpose(1, 2, 0).astype(np.uint8) )
                     print('saved %s' % path)
+
+                    # path = os.path.join('Semantic-Learning-Security-Awareness/semantic_extraction/images/im-epoch-%d-lambda-%d-compre-%d.png' % (
+                    #     e, lambda1, compression_rate))
+                    # # scipy.misc.imsave(path, merged)
+                    # cv2.imwrite(path, im_data[0].transpose(1, 2, 0))
+                    # if not cv2.imwrite(path,  im_data[0].transpose(1, 2, 0)):
+                    #     raise Exception("Could not write image")
                     # #
                     # path = os.path.join('Semantic-Learning-Security-Awareness/semantic_extraction/images/out-epoch-%d-lambda-%d-compre-%d.png' % (
                     #     e, lambda1, compression_rate))
